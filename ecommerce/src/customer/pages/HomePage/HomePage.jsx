6593215c250879d9ab8538f2995fe8959da79100
rohitpahilwan <rohitpahilwan13@gmail.com>
2024-08-09 01:44:20 +0530

    First commit

---
+++ ecommerce/src/customer/pages/HomePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Maincarousel from '../../components/Homcarousel/Maincarousel'
+
+export default function HomePage() {
+  return (
+    <div>
+      <Maincarousel/>
+      <div>
+        Other Section
+      </div>
+    </div>
+  )
+}
